640d281975a2060e7052cc89ca43898d8dd775d0
mb-physik <122852206+mb-physik@users.noreply.github.com>
2023-03-28 00:59:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-var themeSwitcher = document.getElementById("theme-switcher");
 var currentTheme = localStorage.getItem("theme");
+var images = document.getElementsByTagName("img");
 
 if (currentTheme) {
   document.body.classList.add(currentTheme);
-  themeSwitcher.innerHTML = currentTheme === "dark-theme" ? "Tagmodus" : "Nachtmodus";
+  changeImageSources(currentTheme);
+} else {
+  changeImageSources("light-theme");
 }
+
+function changeImageSources(theme) {
+  for (var i = 0; i < images.length; i++) {
+    var src = images[i].getAttribute("src");
+    if (theme === "dark-theme") {
+      images[i].setAttribute("src", src.replace(/(\.[\w\d_-]+)$/i, "_dark$1"));
+    } else {
+      images[i].setAttribute("src", src.replace(/_dark(\.[\w\d_-]+)$/i, "$1"));
+    }
+  }
+}
+
+var themeSwitcher = document.getElementById("theme-switcher");
+themeSwitcher.innerHTML = currentTheme === "dark-theme" ? "Tagmodus" : "Nachtmodus";
 
 themeSwitcher.onclick = function() {
   document.body.classList.toggle("dark-theme");
   var themeName = document.body.classList.contains("dark-theme") ? "dark-theme" : "light-theme";
   localStorage.setItem("theme", themeName);
+  changeImageSources(themeName);
   themeSwitcher.innerHTML = themeName === "dark-theme" ? "Tagmodus" : "Nachtmodus";
 }
 
